dd57a7b939fcaf165a4a23683450ecea3f96cb15
yinjinrun <yinjinrun2005@163.com>
2022-02-11 07:13:06 +0800

    P5666 upd3 not finished

--- Luogu/P5666.cpp
+++ Luogu/P5666.cpp
@@ -30,7 +30,7 @@ template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
 const int N = 1e6 + 10;
 const int K = 20;
 
-int n, siz[N], st[K + 1][N], dfn[N], R[N], tim, son[N], tson[N], top, stk[N], fa[N], tdel[N];
+int n, siz[N], st[K + 1][N], dfn[N], R[N], tim, son[N], tson[N], top, stk[N], fa[N], vson[N], vsiz[N];
 ll ans;
 vec G[N];
 
@@ -38,61 +38,40 @@ void dfs(int x, int p) {
 	dfn[x] = ++tim; son[x] = tson[x] = 0; siz[x] = 1; fa[x] = p;
 	for(auto y : G[x])
 		if(y ^ p) dfs(y, x), siz[x] += siz[y], son[x] = siz[y] > siz[son[x]] ? y : son[x];
-	R[x] = tim;
-}
-
-void rdfs(int x, int p) {
 	top = 0; for(auto y : G[x]) if(y ^ p) stk[++top] = y;
 	sort(stk + 1, stk + top + 1, [&](auto a, auto b) { return siz[a] > siz[b]; });
-	stk[top + 1] = stk[top + 2] = 0; tson[x] = stk[2];
-	//	if(x == 1) tdel[x] = 1e9; else tdel[x] = min(tdel[fa[x]], siz[son[fa[x]]] - siz[x]);
-	tdel[x] = min(tdel[fa[x]], siz[son[x]] - siz[tson[x]]);
-	cerr << x << " " << tdel[x] << " " << endl;
-	for(auto y : G[x]) if(y ^ p) rdfs(y, x);
+	stk[top + 1] = stk[top + 2] = 0; tson[x] = stk[2]; R[x] = tim;
 	st[0][x] = son[x]; rep(i, 1, K) st[i][x] = st[i - 1][st[i - 1][x]];
+	vsiz[x] = siz[x]; vson[x] = son[x];
 }
 
-bool check(int x, int tsiz) { return max(tsiz - siz[x], siz[son[x]]) <= tsiz / 2; }
+bool check(int x, int tsiz) { return max(tsiz - vsiz[x], vsiz[vson[x]]) <= tsiz / 2; }
 
-int calcdn(int x) {
-	int tsiz = siz[x];
-	per(i, K, 0) if(st[i][x] && siz[st[i][x]] >= tsiz / 2) x = st[i][x];
-	return x + (check(fa[x], tsiz) ? fa[x] : 0);
-}
-
-bool intr(int x, int y) { return dfn[x] <= dfn[y] && dfn[y] <= R[x]; }
-
-int calcup(int x) {
-	int tsiz = n - siz[x], lsiz = siz[x], lx = x, res = 0; x = 1;
-	per(i, K, 0) if(st[i][x] && tdel[st[i][x]] >= lsiz) x = st[i][x];
-	if(son[x] != lx) x = son[x];
-	res += (max(tsiz - siz[x], siz[tson[x]]) <= tsiz / 2 ? x : 0) +
-		(max(tsiz - siz[fa[x]], siz[son[fa[x]]] - lsiz) <= tsiz / 2 ? fa[x] : 0);
-	cerr << "!" << lx << " " << x << endl;
-	int tlx = x; x = tson[x]; 
-	per(i, K, 0) if(st[i][x] && siz[st[i][x]] >= tsiz / 2) x = st[i][x];
-	cerr << "!" << lx << " " << x << endl;
-	res += (max(tsiz - siz[x], siz[son[x]]) <= tsiz / 2 ? x : 0) +
-		(fa[x] != tlx && max(tsiz - siz[fa[x]], siz[son[x]]) <= tsiz / 2 ? fa[x] : 0);
-	return res;
+int calcdn(int x, int tsiz) {
+	per(i, K, 0) if(st[i][x] && vsiz[st[i][x]] >= tsiz / 2) x = st[i][x];
+	return (check(x, tsiz) ? x : 0) + (check(fa[x], tsiz) ? fa[x] : 0) + (check(vson[x], tsiz) ? vson[x] : 0);
 }
 
 void vdfs(int x, int p) {
-	if(x != 1) {
-		ans += calcdn(x); ans += calcup(x);
-		//cerr << calcdn(x) << " " << calcup(x) << " " << x << endl;
-	}
-	for(auto y : G[x]) if(y ^ p) vdfs(y, x);
+	for(auto y : G[x]) if(y ^ p) {
+			vsiz[x] = n - siz[y]; vson[x] = son[x] == y ? tson[x] : son[x];
+			if(vsiz[p] > vsiz[vson[x]]) vson[x] = p;
+			st[0][x] = vson[x]; rep(i, 1, K) st[i][x] = st[i - 1][st[i - 1][x]];
+			ans += calcdn(y, vsiz[y]); ans += calcdn(x, vsiz[x]);
+			cerr << calcdn(y, vsiz[y]) << " " << vson[x] << " " << tson[x] << " " << calcdn(x, vsiz[x]) << " " << y << endl;
+			vdfs(y, x);
+		}
+	vsiz[x] = siz[x]; vson[x] = son[x];
+	st[0][x] = vson[x]; rep(i, 1, K) st[i][x] = st[i - 1][st[i - 1][x]];
 }
 
 void solve() {
 	n = in; rep(i, 1, n) G[i].clear(); rep(i, 1, n) rep(j, 0, K) st[j][i] = 0;
-	tdel[0] = 1e9;
 	rep(i, 2, n) {
 		int u = in, v = in;
 		G[u].eb(v); G[v].eb(u);
 	}
-	tim = ans = 0; dfs(1, 0); rdfs(1, 0); vdfs(1, 0);
+	tim = ans = 0; dfs(1, 0); vdfs(1, 0);
 	printf("%lld\n", ans);
 }
 

5b4effe91ed0cb9110397dd949b51c085becf5e6
yinjinrun <yinjinrun2021@outlook.com>
2022-02-16 20:57:29 +0800

    LOJ2270. 「SDOI2017」天才黑客 过了样例！

--- LOJ/2270.cpp
+++ LOJ/2270.cpp
@@ -31,7 +31,7 @@ namespace trie {
 	vec G[N]; int fa[N], dfn[N], tim, st[21][N], dep[N], lg[N], pw[30];
 	int tmn(int x, int y) { return dep[x] < dep[y] ? x : y; }
 	void dfs(int x, int p) {
-		fa[x] = p; dep[x] = dep[p] + 1; st[0][dfn[x] = ++tim] = x;
+		fa[x] = p; dep[x] = dep[p] + 1; st[0][dfn[x] = ++tim] = x; 
 		for(auto y : G[x]) if(y ^ p) dfs(y, x), st[0][++tim] = x;
 	}
 	int lca(int x, int y) {
@@ -43,7 +43,7 @@ namespace trie {
 		rep(i, 1, K - 1) {
 			int u = in, v = in; in;
 			G[u].eb(v);
-		} tim = 0; dfs(1, 0);
+		} tim = 0; dep[0] = -1; dfs(1, 0);
 		rep(i, 2, tim) lg[i] = lg[i >> 1] + 1;
 		pw[0] = 1; rep(i, 1, lg[tim]) pw[i] = pw[i - 1] << 1;
 		rep(k, 1, 20) rep(i, 1, tim - pw[k] + 1) st[k][i] = tmn(st[k - 1][i], st[k - 1][i + pw[k - 1]]);
@@ -52,33 +52,39 @@ namespace trie {
 
 using trie :: lca;
 using trie :: dfn;
+using trie :: dep;
 
 struct edge { int v, w, nxt; } e[N * 10];
-int h[N], cnt, a[N], b[N], c[N], d[N], stk[N], top, dep[N];
-int toid[N], outid[N], ps[N], ss[N];
+int h[N], cnt, a[N], b[N], c[N], d[N], stk[N], top;
+int toid[N], outid[N], ps[N], ss[N], tal[N], outal[N];
 vec tG[N];
 bool vis[N];
 ll dis[N], ans[N];
 
-void link(int x, int y, int w) { e[++cnt] = (edge) { y, w, h[x] }; h[x] = cnt; }
+void link(int x, int y, int w) { /*cerr << "LINK : " << x << " " << y << " " << w << endl, */e[++cnt] = (edge) { y, w, h[x] }; h[x] = cnt; }
 
 void dfs(int x) {
 	toid[x] = ++tot; outid[x] = ++tot;
-	for(auto y : tG[x]) dfs(y);
+	for(auto y : tG[x]) /*cerr << x << "->" << y << endl, */dfs(y);
+	//cerr << "X" << toid[x] << " " << outid[x] << endl;
 	top = 0; for(auto y : tG[x]) stk[++top] = y; // stk is not using
+	//cerr << "PRE: " << endl;
 	rep(i, 1, top) {
 		if(i == 1) ps[i] = toid[stk[i]];
 		else ps[i] = ++tot, link(ps[i], ps[i - 1], 0), link(ps[i], toid[stk[i]], 0);
 	}
+	//cerr << "SUF: " << endl;
 	per(i, top, 1) {
 		if(i == top) ss[i] = toid[stk[i]];
 		else ss[i] = ++tot, link(ss[i], ss[i + 1], 0), link(ss[i], toid[stk[i]], 0);
-	}
+	} //tal[x] = ss[1]; outal[x] = ++tot;
+	//cerr << "FUF: " << endl;
 	rep(i, 1, top) {
-		if(i > 1) link(outid[stk[i]], ps[i - 1], dep[x]);
-		if(i < top) link(outid[stk[i]], ss[i + 1], dep[x]);
+		if(i > 1) link(outid[stk[i]], ps[i - 1], dep[x] * 2);
+		if(i < top) link(outid[stk[i]], ss[i + 1], dep[x] * 2);
 		link(outid[stk[i]], outid[x], 0); link(toid[x], toid[stk[i]], 0);
-	}
+		link(outid[stk[i]], outal[x], 0);
+	} //cerr << "END |" << endl;
 }
 
 void clr(int x) {
@@ -102,20 +108,22 @@ void solve() {
 	rep(i, 1, m) a[i] = in, b[i] = in, c[i] = in, d[i] = in, G[a[i]].eb(i), R[b[i]].eb(i);
 	trie :: init(); for(auto v : G[1]) link(m + 1, v, c[v]);
 	rep(x, 1, n) {
-		vector < int > pot;
+		if(!R[x].size() || !G[x].size()) continue;
+		//cerr << "!" << x << endl;
+		vector < int > pot; 
 		for(auto v : R[x]) pot.eb(d[v]);
 		for(auto v : G[x]) pot.eb(d[v]);
 		sort(pot.begin(), pot.end(), [](auto x, auto y) { return dfn[x] < dfn[y]; });
 		pot.resize(unique(pot.begin(), pot.end()) - pot.begin()); stk[top = 1] = 1;
 		for(auto y : pot) {
 			if(y == 1) continue; int l = lca(y, stk[top]);
-			while(dep[l] <= dep[stk[top - 1]]) tG[stk[top - 1]].eb(stk[top]), top--;
+			while(top > 1 && dep[l] <= dep[stk[top - 1]]) tG[stk[top - 1]].eb(stk[top]), top--;
 			if(l != stk[top]) tG[l].eb(stk[top]), stk[top] = l;
 			stk[++top] = y;
 		} rep(i, 2, top) tG[stk[i - 1]].eb(stk[i]);
 		dfs(1);
-		for(auto v : R[x]) link(v, outid[d[v]], c[v]), link(v, outid[d[v]], c[v] + dep[d[v]]);
-		for(auto v : G[x]) link(toid[d[v]], v, c[v]), link(outid[d[v]], v, c[v] + dep[d[v]]);
+		for(auto v : R[x]) link(v, outid[d[v]], c[v]), link(v, toid[d[v]], c[v] + dep[d[v]] * 2);
+		for(auto v : G[x]) link(toid[d[v]], v, c[v]), link(outid[d[v]], v, c[v] + dep[d[v]] * 2);
 		clr(1);
 	} dijk(m + 1); memset(ans, 0x3f, sizeof ans);
 	rep(i, 1, m) chkmin(ans[b[i]], (dis[i] + c[i]) / 2);

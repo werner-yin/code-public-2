8dcd72835903089f91dabe64dece652b0761d944
yinjinrun <yinjinrun2021@outlook.com>
2022-02-17 15:50:28 +0800

    CF 741D 换了个写法

--- CF/problem/CF741D.cpp
+++ CF/problem/CF741D.cpp
@@ -27,30 +27,41 @@ template < typename T > T read() {
 template < typename T > void chkmax(T &x, const T &y) { x = x > y ? x : y; }
 template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
 
-const int N = 1e6 + 10;
+const int N = 5e5 + 10;
 
 vec G[N];
-int val[N], g[N], t[N], ans[N], n;
-map < int, int > f[N];
+int val[N], siz[N], son[N], ans[N], n, dep[N];
+int mxv[1 << 22], tag;
 
-void merge(int x, int y, int &ans) {
-	if(f[x].size() < f[y].size()) swap(f[x], f[y]), swap(g[x], g[y]), swap(t[x], t[y]);
-	for(auto v : f[y]) {
-		if(f[x].count(v.fi ^ g[y] ^ g[x])) chkmax(ans, f[x][v.fi ^ g[x] ^ g[y]] + t[x] + t[y] + v.se);
-		rep(i, 0, 25)
-			if(f[x].count(v.fi ^ g[y] ^ g[x] ^ (1 << i)))
-				chkmax(ans, f[x][v.fi ^ g[y] ^ g[x] ^ (1 << i)] + t[x] + t[y] + v.se);
-		//chkmax(f[x][v.fi ^ g[y] ^ g[x]], v.se + t[y] - t[x]);
-	}
-	for(auto v : f[y]) chkmax(f[x][v.fi ^ g[y] ^ g[x]], v.se + t[y] - t[x]);
+void dfs(int x, int p) {
+	siz[x] = 1; dep[x] = dep[p] + 1;
+	for(auto y : G[x]) dfs(y, x), son[x] = siz[y] > siz[son[x]] ? y : son[x], siz[x] += siz[y];
 }
 
-void dfs(int x) {
-	for(auto y : G[x]) dfs(y), chkmax(ans[x], ans[y]);
-	g[x] = 0; f[x][0] = 0;
-	cerr << x << " " << ans[x] << endl;
-	for(auto y : G[x]) t[y]++, g[y] ^= 1 << val[y], merge(x, y, ans[x]);
-	cerr << x << " " << ans[x] << endl;
+void query(int x, int res, int &ans, int tdep) {
+	chkmax(ans, mxv[res ^ tag] + dep[x] - tdep * 2);
+	rep(i, 0, 21) chkmax(ans, mxv[res ^ tag ^ (1 << i)] + dep[x] - tdep * 2);
+	for(auto y : G[x]) 
+		query(y, res ^ 1 << val[y], ans, tdep);
+}
+
+void add(int x, int res, int tdep) {
+	for(auto y : G[x]) add(y, res ^ 1 << val[y], tdep);
+	chkmax(mxv[res ^ tag], dep[x]);
+}
+
+void remove(int x, int res) {
+	for(auto y : G[x]) remove(y, res ^ 1 << val[y]);
+	mxv[res ^ tag] = -1e9;
+}
+
+void tdfs(int x, int fl) {
+	for(auto y : G[x]) if(y ^ son[x]) tdfs(y, 0), chkmax(ans[x], ans[y]);
+	if(son[x]) tdfs(son[x], 1), chkmax(ans[x], ans[son[x]]);
+	tag ^= 1 << val[son[x]]; chkmax(ans[x], mxv[tag] - dep[x]); rep(i, 0, 21) chkmax(ans[x], mxv[tag ^ (1 << i)] - dep[x]);
+	chkmax(mxv[tag], dep[x]);
+	for(auto y : G[x]) if(y ^ son[x]) query(y, 1 << val[y], ans[x], dep[x]), add(y, 1 << val[y], dep[x]);
+	if(fl == 0) remove(x, 0), tag = 0;
 }
 
 int main() {
@@ -58,6 +69,7 @@ int main() {
 	freopen("1.in", "r", stdin);
 #endif
 	n = in; rep(i, 2, n) { G[in].eb(i); char t = getchar(); while(!isalpha(t)) t = getchar(); val[i] = t - 'a'; }
-	dfs(1);  rep(i, 1, n) printf("%d ", ans[i]); puts("");
+	memset(mxv, 0xcf, sizeof mxv);
+	dfs(1, 0); tdfs(1, 1); rep(i, 1, n) printf("%d ", ans[i]); puts("");
 	return 0;
 }

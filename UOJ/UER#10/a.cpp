6d50051142c63f5dccdc4de6e59eb1fdb4f93937
yinjinrun <yinjinrun2005@163.com>
2022-04-25 16:56:55 +0800

    UER#10A.cpp 通过了 Hack 数据！

--- UOJ/UER#10/a.cpp
+++ UOJ/UER#10/a.cpp
@@ -2,13 +2,12 @@
 
 using namespace std;
 
-long double t1, t2, a;
+double t1, t2, a, tot;
 
 int main() {
 	int n; scanf("%d", &n);
 	for(int i = 1; i <= n; i++) {
-		scanf("%Lf", &a); a /= 1000000;
-		t1 += 1. * (1 - a) / (n - a);
-		t2 += 1. * (n - 1) / n / (n - a);
-	} printf("%.14Lf\n", t2 / (1 - t1));
+		scanf("%lf", &a); a /= 1000000;
+		tot += a / (n - a);
+	} tot = 1. / n + 1 / tot; printf("%.14lf\n", tot);
 }

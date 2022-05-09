3300fb3703062387217189f22381c17bc2807278
yinjinrun <yinjinrun2005@163.com>
2022-05-09 12:07:30 +0800

    CF702F

--- CF/problem/CF702F.cpp
+++ CF/problem/CF702F.cpp
@@ -29,60 +29,58 @@ template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
 
 const int N = 1e6 + 10;
 
-int n, c[N], q[N], id[N], rt[N], tim, ans[N];
+int n, c[N], q[N], id[N], ans[N], m;
 
 mt19937_64 rnd(19491001);
 
 namespace tr {
-	const int T = 2.5e7 + 10;
-	struct node {
-		int ch[2], tvis, val;
-		ll len, siz, ad;
-	} val[T];
-	int ndn;
-	void pu(int x) { val[x].siz = val[val[x].ch[0]].siz + val[val[x].ch[1]].siz + val[x].len; }
-	int nd(int v, int tv) { int x = ++ndn; val[x].len = v, val[x].val = tv; pu(x); return x; }
-	int copy(int y) { if(val[y].tvis == tim) return y; int x = ++ndn; val[x] = val[y]; val[x].tvis = tim; pu(x); return x; }
-	void pt(int &x, ll v) { if(!x) return; x = copy(x); val[x].val += v; val[x].ad += v; }
-	void pd(int &x) { if(!x) return; x = copy(x); if(val[x].ad) pt(val[x].ch[0], val[x].ad), pt(val[x].ch[1], val[x].ad), val[x].ad = 0; }
-	int merge(int x, int y) {
-		if(!x || !y) return x | y; pd(x), pd(y); if(val[x].siz + val[y].siz == 0) { puts("0000!"), exit(0); }
-		if(rnd() % (val[x].siz + val[y].siz) < val[x].siz) return val[x].ch[1] = merge(val[x].ch[1], y), pu(x), x;
-		else return val[y].ch[0] = merge(x, val[y].ch[0]), pu(y), y;
+	int ch[N][2], val[N], ret[N], siz[N], ad1[N], ad2[N], ndn;
+	int nd(int v) { int x = ++ndn; val[x] = v; siz[x] = 1; ret[x] = 0; return x; }
+	void pu(int x) { siz[x] = siz[ch[x][0]] + siz[ch[x][1]] + 1; }
+	void pt(int x, int v, int tv) { ad1[x] += v, ad2[x] += tv, val[x] += v, ret[x] += tv; }
+	void pd(int x) {
+		pt(ch[x][0], ad1[x], ad2[x]), pt(ch[x][1], ad1[x], ad2[x]), ad1[x] = ad2[x] = 0;
 	}
-	void split(int cur, ll k, int &x, int &y) {
+	int merge(int x, int y) {
+		if(!x || !y) return x | y; pd(x), pd(y);
+		if(rnd() % (siz[x] + siz[y]) < siz[x]) return ch[x][1] = merge(ch[x][1], y), pu(x), x;
+		else return ch[y][0] = merge(x, ch[y][0]), pu(y), y;
+	}
+	void split(int cur, int k, int &x, int &y) {
 		if(!cur) return x = y = 0, void(); pd(cur);
-		if(val[val[cur].ch[0]].siz + val[cur].len <= k)
-			x = cur, split(val[cur].ch[1], k - val[val[cur].ch[0]].siz - val[cur].len, val[x].ch[1], y), pu(x);
-		else if(val[val[cur].ch[0]].siz == k)
-			x = copy(val[cur].ch[0]), y = cur, val[y].ch[0] = 0, pu(x), pu(y);
-		else if(val[val[cur].ch[0]].siz <= k)
-			x = nd(k - val[val[cur].ch[0]].siz, val[cur].val), y = cur, val[y].len = val[cur].len - val[x].len,
-				val[x].ch[0] = val[cur].ch[0], val[y].ch[0] = 0, pu(x), pu(y);
-		else 
-			y = cur, split(val[cur].ch[0], k, x, val[y].ch[0]), pu(y);
+		if(val[cur] <= k) x = cur, split(ch[cur][1], k, ch[x][1], y), pu(x);
+		else y = cur, split(ch[cur][0], k, x, ch[y][0]), pu(y);
+	}
+	void expand(int x, vec &pot) {
+		if(!x) return; pot.eb(x); pd(x); expand(ch[x][0], pot); expand(ch[x][1], pot); ch[x][0] = ch[x][1] = 0; pu(x);
+	}
+	int ins(int x, int v) {
+		int a, b; split(x, val[v], a, b); 
+		x = merge(a, merge(v, b)); return x;
+	}
+	void getall(int x) {
+		if(!x) return; pd(x); getall(ch[x][0]); getall(ch[x][1]);
 	}
 }
 
 using namespace tr;
+
+int rt;
+vec pot;
 
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("1.in", "r", stdin);
 #endif
 	n = in; rep(i, 1, n) c[i] = in, q[i] = in, id[i] = i;
-	sort(id + 1, id + n + 1, [&](int x, int y) { return q[x] ^ q[y] ? q[x] < q[y] : c[x] > c[y]; });
-	int a, b; rt[0] = nd(1e9 + 7, 0);
+	sort(id + 1, id + n + 1, [&](int x, int y) { return q[x] ^ q[y] ? q[x] > q[y] : c[x] < c[y]; });
+	m = in; rep(i, 1, m) rt = ins(rt, nd(in));
+	int a, b, d, e;
 	rep(i, 1, n) {
-		int x = id[i]; rt[i] = rt[i - 1]; tim = i;
-		split(rt[i], c[x], a, b); pt(rt[i - 1], 1); rt[i] = merge(a, rt[i - 1]);
-	} 
-	int m = in, c;
-	auto query = [&](int t) {
-					 split(rt[n], t, a, b); split(b, 1, b, c);
-					 int ans = val[b].val; rt[n] = merge(a, merge(b, c));
-					 return ans;
-				 };
-	rep(i, 1, m) printf("%d ", query(in)); puts("");
+		int x = id[i]; split(rt, c[x] - 1, a, b), split(b, c[x] * 2, d, e);
+		pot.clear(); expand(d, pot); rt = a; for(auto v : pot) val[v] -= c[x], ret[v]++, rt = ins(rt, v);
+		pt(e, -c[x], 1), rt = merge(rt, e); 
+	} getall(rt); 
+	rep(i, 1, m) printf("%d ", ret[i]); puts("");
 	return 0;
 }

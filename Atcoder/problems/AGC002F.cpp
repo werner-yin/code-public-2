b88388132430e86b97f7ed7ae681220f8d03f1a8
yinjinrun <yinjinrun2005@163.com>
2022-05-11 11:29:45 +0800

    AGC002F

---
+++ Atcoder/problems/AGC002F.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#define eb emplace_back
+#define ep emplace
+#define fi first
+#define se second
+#define in read<int>()
+#define lin read<ll>()
+#define rep(i, x, y) for(int i = (x); i <= (y); i++)
+#define per(i, x, y) for(int i = (x); i >= (y); i--)
+
+using namespace std;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using vec = vector < int >;
+using veg = vector < pii >;
+
+template < typename T > T read() {
+	T x = 0; bool f = 0; char ch = getchar();
+	while(!isdigit(ch)) f |= ch == '-', ch = getchar();
+	while(isdigit(ch)) x = x * 10 + (ch ^ 48), ch = getchar();
+	return f ? -x : x;
+}
+
+template < typename T > void chkmax(T &x, const T &y) { x = x > y ? x : y; }
+template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
+
+const int N = 2e3 + 10;
+//constexpr int mod = 998244353;
+constexpr int mod = 1e9 + 7;
+
+int reduce(int x) {
+	if(x < 0) x += mod;
+	if(x >= mod) x -= mod;
+	return x;
+}
+
+template < typename T > T qp(T x, ll t) { T res = 1; for(; t; t >>= 1, x *= x) if(t & 1) res *= x; return res; }
+
+struct Z { // modint
+	int x;
+	Z(int x = 0) : x(reduce(x)) {}
+	Z(ll x) : x(reduce(x % mod)) {}
+	Z operator -() const { return Z(reduce(mod - x)); }
+	int val() { return x; }
+	Z inv() const { assert(x); return qp(*this, mod - 2); }
+	Z &operator += (const Z &t) { x = reduce(x + t.x); return *this; }
+	Z &operator -= (const Z &t) { x = reduce(x - t.x); return *this; }
+	Z &operator *= (const Z &t) { x = (ll)x * t.x % mod; return *this; }
+	Z &operator /= (const Z &t) { return *this *= t.inv(); }
+	friend Z operator + (const Z &a, const Z &b) { Z res = a; res += b; return res; }
+	friend Z operator - (const Z &a, const Z &b) { Z res = a; res -= b; return res; }
+	friend Z operator * (const Z &a, const Z &b) { Z res = a; res *= b; return res; }
+	friend Z operator / (const Z &a, const Z &b) { Z res = a; res /= b; return res; }
+};
+
+Z fac[N * N], ifac[N * N];
+Z C(int x, int y) { return x < 0 || y < 0 || x < y ? Z(0) : fac[x] * ifac[y] * ifac[x - y]; }
+void init(int l) {
+	fac[0] = 1; rep(i, 1, l) fac[i] = fac[i - 1] * Z(i); ifac[l] = fac[l].inv();
+	per(i, l - 1, 0) ifac[i] = ifac[i + 1] * Z(i + 1);
+}
+
+int n, K;
+Z f[N][N];
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("1.in", "r", stdin);
+#endif
+	n = in, K = in; init(n * K);
+	if(K == 1) return puts("1"), 0;
+	rep(i, 1, n) f[i][0] = 1;
+	rep(i, 1, n) rep(j, 1, i)
+		f[i][j] = f[i - 1][j] + f[i][j - 1] * (n - j + 1) * C(n * K - i - (j - 1) * (K - 1) - 1, K - 2);
+	cout << f[n][n].val() << endl;
+	return 0;
+}

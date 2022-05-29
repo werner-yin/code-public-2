1114c5b25aa6f69cb7a63dbc94f1b68db60fe7ca
yinjinrun <yinjinrun2005@163.com>
2022-05-29 11:55:46 +0800

    AGC040D

--- Atcoder/problems/AGC040D.cpp
+++ Atcoder/problems/AGC040D.cpp
@@ -30,7 +30,7 @@ template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
 const int N = 1e6 + 10;
 
 int n;
-ll sb[N], sa[N];
+ll sb[N], sa[N], sc[N], tota;
 pii val[N];
 
 struct frac {
@@ -38,42 +38,39 @@ struct frac {
 	void reduce() { ll t = __gcd(x, y); x /= t, y /= t; }
 	frac(ll x = 0, ll y = 1) : x(x), y(y) { reduce(); }
 	friend frac operator + (frac a, ll b) { return frac(a.x + b * a.y, a.y); }
-	friend bool operator < (frac a, frac b) { return a.x * b.y < a.y * b.x; }
+	friend bool operator < (frac a, frac b) { return (__int128)a.x * b.y < (__int128)a.y * b.x; }
 } ans;
 
-ll tr[N];
-void add(int x, ll v) { for(; x <= n; x += x & -x) tr[x] += v; }
-ll query(int x) { ll ret = 0; for(; x; x -= x & -x) ret += tr[x]; return ret; }
-int ttr[N];
-void alef(int x, int v) { for(; x <= n; x += x & -x) ttr[x] += v; }
-int qlef(int x) { int ret = 0; for(; x; x -= x & -x) ret += ttr[x]; return ret; }
+ll calc(int x, int y) {
+	ll t = max(val[x].fi, val[x].se), ret = sc[y];
+	if(max(val[y].fi, val[y].se) <= t) ret -= t; return ret;
+}
+
+int get(int x) {
+	int l = 1, r = n, ans = -1;
+	while(l <= r) {
+		int mid = l + r >> 1;
+		if(calc(x, mid) >= tota - val[x].se) ans = mid, r = mid - 1;
+		else l = mid + 1;
+	} return ans;
+}
 
 int main() {
 #ifdef YJR_2333_TEST
 	freopen("1.in", "r", stdin);
 #endif
-	n = in; rep(i, 1, n) val[i].fi = in, val[i].se = in;
-	sort(val + 1, val + n + 1, [&](auto x, auto y) { return max(x.fi, x.se) < max(y.fi, y.se);return x.se ^ y.se ? x.se < y.se : x.fi < y.fi; });
-	rep(i, 1, n) sa[i] = sa[i - 1] + val[i].fi, sb[i] = sb[i - 1] + val[i].se, add(i, val[i].se), alef(i, 1);
-	ll tota = sa[n], totb = sb[n]; int cur = 0;
-	//rep(i, 1, n) cerr << val[i].fi << " " ; cerr << endl;
-	//rep(i, 1, n) cerr << val[i].se << " " ; cerr << endl;
-	per(i, n, 0) {
-		//while(cur > i) cur--;
-		while(cur < n && totb - query(cur + 1) >= tota) cur++;
-		//cerr << i << " " << cur << " " << tota << " " << totb << endl;
-		//if(cur > i) break;
-		//assert(totb - query(cur) - tota >= 0);
-		frac tans;
-		if(cur + 1 <= n) tans = frac(max((totb - query(cur)) - tota, (ll)0), val[cur + 1].se);
-		//cerr << tans.x << " " << tans.y << endl;
-		tans = tans + qlef(cur); tans.y = tans.y * n; tans.reduce(); chkmax(ans, tans); 
-		if(i && val[i].fi > val[i].se) {
-			tota -= val[i].fi, totb -= val[i].se, add(i, -val[i].se); val[i].se = 0; alef(i, -1);
-		}
-	}
-	function< void(ll) > twrite = [&](ll x) { if(x >= 10) twrite(x / 10); putchar(x % 10 + '0'); };
-	auto write = [&](ll t) { if(t == 0) putchar('0'); else assert(t >= 0), twrite(t); };
-	write(ans.x), putchar(' '), write(ans.y);
+	n = in; ans = frac(n, 1); rep(i, 1, n) val[i].fi = in, val[i].se = in;
+	sort(val + 1, val + n + 1, [&](auto x, auto y) { return max(x.fi, x.se) > max(y.fi, y.se); });
+	rep(i, 1, n) sa[i] = sa[i - 1] + val[i].fi, sb[i] = sb[i - 1] + val[i].se, sc[i] = sc[i - 1] + max(val[i].fi, val[i].se);
+	tota = sa[n];
+	rep(i, 1, n) {
+		int pos = get(i); if(pos < 0) continue; ll ret = calc(i, pos); if(ret > tota) continue;
+		if(max(val[pos].fi, val[pos].se) <= max(val[i].fi, val[i].se)) pos--;
+		frac tans(tota - ret, val[i].se); tans = tans + pos; chkmin(ans, tans);
+#ifdef YJR_2333_TEST
+		cerr << pos << " " << ret << " " << tota << " " << tans.x << " " << tans.y << endl;
+#endif
+	} ans.y *= n; ans.x = ans.y - ans.x; ans.reduce();
+	cout << ans.x << " " << ans.y << endl;
 	return 0;
 }

54e8b257a325d8213575906670c52153b795559c
yinjinrun <yinjinrun2005@163.com>
2022-06-15 14:30:23 +0800

    AGC009C

---
+++ Atcoder/problems/AGC009C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define eb emplace_back
+#define ep emplace
+#define fi first
+#define se second
+#define in read<int>()
+#define lin read<ll>()
+#define rep(i, x, y) for(int i = (x); i <= (y); i++)
+#define per(i, x, y) for(int i = (x); i >= (y); i--)
+
+using namespace std;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using vec = vector < int >;
+using veg = vector < pii >;
+
+template < typename T > T read() {
+	T x = 0; bool f = 0; char ch = getchar();
+	while(!isdigit(ch)) f |= ch == '-', ch = getchar();
+	while(isdigit(ch)) x = x * 10 + (ch ^ 48), ch = getchar();
+	return f ? -x : x;
+}
+
+template < typename T > void chkmax(T &x, const T &y) { x = x > y ? x : y; }
+template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
+
+const int N = 1e6 + 10;
+//constexpr int mod = 998244353;
+constexpr int mod = 1e9 + 7;
+
+int reduce(int x) {
+	if(x < 0) x += mod;
+	if(x >= mod) x -= mod;
+	return x;
+}
+
+template < typename T > T qp(T x, ll t) { T res = 1; for(; t; t >>= 1, x *= x) if(t & 1) res *= x; return res; }
+
+struct Z { // modint
+	int x;
+	Z(int x = 0) : x(reduce(x)) {}
+	Z(ll x) : x(reduce(x % mod)) {}
+	Z operator -() const { return Z(reduce(mod - x)); }
+	int val() { return x; }
+	Z inv() const { assert(x); return qp(*this, mod - 2); }
+	Z &operator += (const Z &t) { x = reduce(x + t.x); return *this; }
+	Z &operator -= (const Z &t) { x = reduce(x - t.x); return *this; }
+	Z &operator *= (const Z &t) { x = (ll)x * t.x % mod; return *this; }
+	Z &operator /= (const Z &t) { return *this *= t.inv(); }
+	friend Z operator + (const Z &a, const Z &b) { Z res = a; res += b; return res; }
+	friend Z operator - (const Z &a, const Z &b) { Z res = a; res -= b; return res; }
+	friend Z operator * (const Z &a, const Z &b) { Z res = a; res *= b; return res; }
+	friend Z operator / (const Z &a, const Z &b) { Z res = a; res /= b; return res; }
+};
+
+Z fac[N], ifac[N];
+Z C(int x, int y) { return x < 0 || y < 0 || x < y ? Z(0) : fac[x] * ifac[y] * ifac[x - y]; }
+void init(int l) {
+	fac[0] = 1; rep(i, 1, l) fac[i] = fac[i - 1] * Z(i); ifac[l] = fac[l].inv();
+	per(i, l - 1, 0) ifac[i] = ifac[i + 1] * Z(i + 1);
+}
+
+Z f[N];
+int n;
+ll A, B, s[N];
+queue < int > q1, q2;
+Z tot;
+
+int main() {
+#ifdef YJR_2333_TEST
+	freopen("1.in", "r", stdin);
+#endif
+	n = in, A = lin, B = lin; if(A < B) swap(A, B);
+	rep(i, 1, n) s[i] = lin;
+	s[0] = -1e18; s[n + 1] = 2e18; f[0] = 1; q1.ep(0), tot = 1;
+	rep(i, 1, n + 1) {
+		while(q2.size() && s[i] - s[q2.front()] >= A) q1.push(q2.front()), tot += f[q2.front()], q2.pop();
+		f[i] = tot;
+		if(s[i + 1] - s[i - 1] >= B) q2.push(i);
+		if(s[i] - s[i - 1] < B) {
+			while(q1.size() && q1.front() < i - 1) tot -= f[q1.front()], q1.pop();
+			while(q2.size() && q2.front() < i - 1) q2.pop();
+		}
+	} printf("%d\n", f[n + 1].val());
+	return 0;
+}

74b713587206ff3da9a764e0b0cc90c9633cecc6
yinjinrun <yinjinrun2005@163.com>
2022-06-22 11:45:40 +0800

    AGC010D not finished

---
+++ Atcoder/problems/AGC010D.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define eb emplace_back
+#define ep emplace
+#define fi first
+#define se second
+#define in read<int>()
+#define lin read<ll>()
+#define rep(i, x, y) for(int i = (x); i <= (y); i++)
+#define per(i, x, y) for(int i = (x); i >= (y); i--)
+
+using namespace std;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using vec = vector < int >;
+using veg = vector < pii >;
+
+template < typename T > T read() {
+	T x = 0; bool f = 0; char ch = getchar();
+	while(!isdigit(ch)) f |= ch == '-', ch = getchar();
+	while(isdigit(ch)) x = x * 10 + (ch ^ 48), ch = getchar();
+	return f ? -x : x;
+}
+
+template < typename T > void chkmax(T &x, const T &y) { x = x > y ? x : y; }
+template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
+
+const int N = 1e6 + 10;
+
+int n, a[N];
+ll tot;
+
+int main() {
+#ifdef YJR_2333_TEST
+	freopen("1.in", "r", stdin);
+#endif
+	n = in; rep(i, 1, n) a[i] = in, tot += a[i] - 1;
+	if(tot % 2 == 0) puts("Second");
+	else puts("First");
+	return 0;
+}

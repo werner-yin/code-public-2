cb3d23ed670ebca172900d265b7dfeb2fc50ba45
yinjinrun <yinjinrun2005@163.com>
2022-07-12 11:00:29 +0800

    AGC029 Finished!

---
+++ Atcoder/problems/AGC029A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define eb emplace_back
+#define ep emplace
+#define fi first
+#define se second
+#define in read<int>()
+#define lin read<ll>()
+#define rep(i, x, y) for(int i = (x); i <= (y); i++)
+#define per(i, x, y) for(int i = (x); i >= (y); i--)
+
+using namespace std;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using vec = vector < int >;
+using veg = vector < pii >;
+
+template < typename T > T read() {
+	T x = 0; bool f = 0; char ch = getchar();
+	while(!isdigit(ch)) f |= ch == '-', ch = getchar();
+	while(isdigit(ch)) x = x * 10 + (ch ^ 48), ch = getchar();
+	return f ? -x : x;
+}
+
+template < typename T > void chkmax(T &x, const T &y) { x = x > y ? x : y; }
+template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
+
+const int N = 1e6 + 10;
+
+char s[N];
+int n;
+
+int main() {
+#ifdef YJR_2333_TEST
+	freopen("1.in", "r", stdin);
+#endif
+	scanf("%s", s + 1); n = strlen(s + 1);
+	int cur = n; ll ans = 0;
+	per(i, n, 1) {
+		if(s[i] == 'B') {
+			ans += cur - i; cur--;
+		}
+	} cout << ans << endl;
+	return 0;
+}

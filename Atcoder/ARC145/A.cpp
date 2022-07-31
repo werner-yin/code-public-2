957e5bdcbbc45f344b3e672fe1ae8d7d76001bb1
yinjinrun <yinjinrun2005@163.com>
2022-07-31 14:33:16 +0800

    AGC145A

--- Atcoder/ARC145/A.cpp
+++ Atcoder/ARC145/A.cpp
@@ -30,37 +30,14 @@ template < typename T > void chkmin(T &x, const T &y) { x = x < y ? x : y; }
 const int N = 1e6 + 10;
 
 int n;
-char s[N], t[N];
-
-bool check1() {
-	rep(i, 1, n) {
-		int t = n + 1 - i;
-		if(s[i] == s[t]) continue;
-		if(s[i] == 'A') return 0;
-		s[i] = 'A'; s[i + 1] = 'B';
-		if(s[i] != s[t]) return 0;
-	} return 1;
-}
-
-bool check2() {
-	rep(i, 1, n) {
-		int t = n + 1 - i;
-		if(s[i] == s[t]) continue;
-		if(s[i] == 'A') return 0;
-		s[t] = 'B', s[t - 1] = 'A';
-		if(s[i] != s[t]) return 0;
-	} return 1;
-}
 
 int main() {
 #ifdef YJR_2333_TEST
 	freopen("1.in", "r", stdin);
 #endif
-	n = in; scanf("%s", s + 1);
-	rep(i, 1, n) t[i] = s[i];
-	if(check1()) return puts("Yes"), 0;
-	rep(i, 1, n) s[i] = t[i];
-	if(check2()) return puts("Yes"), 0;
-	puts("No");
+	n = in;
+	string s; cin >> s;
+	if(!(s.front() == 'A' && s.back() == 'B') && s != "BA") puts("Yes");
+	else puts("No");
 	return 0;
 }
